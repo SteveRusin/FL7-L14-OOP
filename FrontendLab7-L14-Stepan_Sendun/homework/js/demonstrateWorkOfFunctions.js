d3e652b1752a60a46d9ae1f9e9647462a8f21750
SteveRusin <steve.neeson21@gmail.com>
2017-07-31 22:56:57 +0300

    1

--- FrontendLab7-L14-Stepan_Sendun/homework/js/demonstrateWorkOfFunctions.js
+++
@@ -1,7 +0,0 @@
-var isBigger = require('main.js');
-
-var game = new Casino(7, 59782);
-
-game.getTotalMoneyInCasino().getNumberOfMachinesInCasino().deleteMachine(2).getNumberOfMachinesInCasino().setNewMachine().getTotalMoneyInCasino().takeMoneyFromCasino(2565).getTotalMoneyInCasino();
-
-game.machines[0].getTotalMoneyInMachine().putMoney(5833).getTotalMoneyInMachine().takeMoney(2658).getTotalMoneyInMachine().play(2015);
